af9e76d8fc4d5ec02bc02a35efcb1a7535a992f4
Michele Gruppioni <gruppiofigo@hotmail.it>
2019-10-31 10:48:32 +0100

    Initial Commit

---
+++ DJ DrumTests/DJ_DrumTests.swift
@@ -0,0 +1,34 @@
+//
+//  DJ_DrumTests.swift
+//  DJ DrumTests
+//
+//  Created by Michele Gruppioni on 31/10/2019.
+//  Copyright © 2019 Michele Gruppioni. All rights reserved.
+//
+
+import XCTest
+@testable import DJ_Drum
+
+class DJ_DrumTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
